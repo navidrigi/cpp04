8c0bf7dfd9f136c8f4465464ced9ba5ba0fbf832
navid <navid.rigi23@gmail.com>
2024-03-13 16:18:48 +0100

    initial files

---
+++ ex00/Cat.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+	public:
+};
---
+++ ex00/Dog.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+	public:
+};
