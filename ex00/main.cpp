a4a5ac3f4525781cf9de0eb884c6ccfd8957e213
navid <navid.rigi23@gmail.com>
2024-03-14 10:02:02 +0100

    added new main with more tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,19 +4,60 @@
 
 int main()
 {
-	const WrongAnimal* meta = new WrongAnimal();
-	const Animal* j = new Dog();
-	const WrongAnimal* i = new WrongCat();
+	const int numAnimals = 10;
+	Animal* animals[numAnimals];
+	WrongAnimal* wrongAnimals[numAnimals];
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
+	for (int i = 0; i < numAnimals; ++i)
+	{
+		if (i % 2 == 0)
+		{
+			animals[i] = new Dog();
+			wrongAnimals[i] = new WrongCat();
+		}
+		else
+		{
+			animals[i] = new Cat();
+			wrongAnimals[i] = new WrongAnimal();
+		}
+	}
 
-	delete j;
-	delete i;
-	delete meta;
+	for (int i = 0; i < numAnimals; ++i)
+	{
+		std::cout << "Animal " << i << " is a " << animals[i]->getType() << std::endl;
+		animals[i]->makeSound();
+		std::cout << "WrongAnimal " << i << " is a " << wrongAnimals[i]->getType() << std::endl;
+		wrongAnimals[i]->makeSound();
+	}
+
+	for (int i = 0; i < numAnimals; ++i)
+	{
+		delete animals[i];
+		delete wrongAnimals[i];
+	}
 
 	return 0;
 }
+
+// #include "Cat.hpp"
+// #include "Dog.hpp"
+// #include "WrongCat.hpp"
+
+// int main()
+// {
+// 	const WrongAnimal* meta = new WrongAnimal();
+// 	const Animal* j = new Dog();
+// 	const WrongAnimal* i = new WrongCat();
+
+// 	std::cout << j->getType() << " " << std::endl;
+// 	std::cout << i->getType() << " " << std::endl;
+// 	i->makeSound(); // will output the cat sound!
+// 	j->makeSound(); // will output the cat sound!
+// 	meta->makeSound(); // will output animal sound!
+
+// 	delete j;
+// 	delete i;
+// 	delete meta;
+
+// 	return 0;
+// }
